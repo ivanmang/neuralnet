05cb75854819fe76a7df2731f844d3ca7e1bf7c7
Ivan Mang <hxm16@ic.ac.uk>
2019-02-24 19:06:30 +0000

    Minor changes in MSE

--- learn_FM.py
+++ learn_FM.py
@@ -1,4 +1,5 @@
 import numpy as np
+# from sklearn.metrics import accuracy_score
 
 from nn_lib import (
     MultiLayerNetwork,
@@ -8,7 +9,9 @@ from nn_lib import (
     load_network,
 )
 from illustrate import illustrate_results_FM
+def evaluate_architecture(predict, actual):
 
+    pass
 
 def main():
     dataset = np.loadtxt("FM_dataset.dat")
@@ -17,8 +20,8 @@ def main():
     #######################################################################
 
     input_dim = 3
-    neurons = [16, 3]
-    activations = ["relu", "identity"]
+    neurons = [16,32,3]
+    activations = ["relu","relu","identity"]
     network = MultiLayerNetwork(input_dim, neurons, activations)
 
     np.random.shuffle(dataset)
@@ -28,12 +31,15 @@ def main():
     x = dataset[:, :3]
     y = dataset[:, 3:]
 
-    split_idx = int(0.8 * len(x))
+    train_split_idx = int(0.8 * len(x))
+    val_split_idx = int(0.1 * len(x))
 
-    x_train = x[:split_idx]
-    y_train = y[:split_idx]
-    x_val = x[split_idx:]
-    y_val = y[split_idx:]
+    x_train = x[:train_split_idx]
+    y_train = y[:train_split_idx]
+    x_val = x[val_split_idx:train_split_idx]
+    y_val = y[val_split_idx:train_split_idx]
+    x_test = x[val_split_idx:]
+    y_test = y[val_split_idx:]
 
     x_train_pre = prep.apply(x_train)
     x_val_pre = prep.apply(x_val)
@@ -48,13 +54,13 @@ def main():
     )
 
     trainer.train(x_train_pre, y_train)
-    print("Train loss = ", trainer.eval_loss(x_train_pre, y_train))
-    print("Validation loss = ", trainer.eval_loss(x_val_pre, y_val))
 
-    preds = network(x_val_pre).argmax(axis=1).squeeze()
-    targets = y_val.argmax(axis=1).squeeze()
-    accuracy = (preds == targets).mean()
-    print("Validation accuracy: {}".format(accuracy))
+    preds = network(x_val_pre).squeeze()
+    # print(preds)
+    targets = y_val.squeeze()
+    # print(targets)
+    accuracy = ((preds - targets)**2).mean()
+    print("MSE: {}".format(accuracy/len(preds)))
 
     #######################################################################
     #                       ** END OF YOUR CODE **
